a6283d4262f8b377cb14f7507a30c6dce721ffa8
ls-triamec <larissa.stadelmann@triamec.com>
2025-05-30 17:11:20 +0200

    reads header from csv file

---
+++ TraSt-CSV/CsvParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraSt_CSV {
+    internal class CsvParser {
+
+        public string Profile { get; private set; }
+        public double SamplingTime { get; private set; }
+        public List<string> columnsName { get; private set; } = new List<string>();
+
+        public void ParseHeader(string filePath) {
+            foreach (var line in File.ReadLines(filePath)) {
+                string trimmedLine = line.Trim();
+
+                // Wenn die Zeile nicht mit "//" beginnt, dann ist der Header zu Ende und wir beenden die Schleife
+                if (!trimmedLine.StartsWith("//")) {
+                    break;
+                }
+
+                // Profile auslesen
+                if (trimmedLine.StartsWith("// Profile:")) {
+                    Profile = trimmedLine.Substring("// Profile:".Length).Trim();
+                }
+                // Sampling Time auslesen
+                else if (trimmedLine.StartsWith("// SamplingTime:")) {
+                    string samplingStr = trimmedLine.Substring("// SamplingTime:".Length).Trim();
+                    if (samplingStr.EndsWith("s")) {
+                        samplingStr = samplingStr.Substring(0, samplingStr.Length - 1);
+                    }
+                } 
+                // Spaltennamen auslesen
+                else if (trimmedLine.StartsWith("// Column")) {
+                    int colonIndex = trimmedLine.IndexOf(':');
+                    if(colonIndex != -1 && (colonIndex + 1 < trimmedLine.Length)) {
+                        string columnName = trimmedLine.Substring(colonIndex + 1).Trim();
+                        columnsName.Add(columnName);
+                    }
+                }
+
+            }
+        }
+    }
+}
+
--- TraSt-CSV/Program.cs
+++ TraSt-CSV/Program.cs
@@ -1,7 +1,26 @@
-﻿namespace TraSt_CSV {
+﻿// Copyright © 2025 Triamec Motion AG
+using Triamec.Tam;
+using Triamec.TriaLink.Adapter;
+
+namespace TraSt_CSV {
     internal class Program {
+        static string _filePath = "TrajectoryStream_Test1.csv";
+        
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            CsvParser _csvParser = new CsvParser();
+
+            _csvParser.ParseHeader(_filePath);
+
+            Console.WriteLine("The Header of the file was read and the following columns were found: ");
+            foreach( var column in _csvParser.columnsName) {
+                Console.WriteLine("- " +column);
+            }
+
+            Console.WriteLine($"Connecting with {_csvParser.columnsName.Count} axes to match the number of columns.");
+
         }
+
+
     }
 }
